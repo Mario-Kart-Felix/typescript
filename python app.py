41322e8cfeeca85a0c1debff8de2e68eeab05642
Mario-Kart-Felix <76971465+Mario-Kart-Felix@users.noreply.github.com>
2021-04-25 03:52:03 -0500

    Create python app.py

---
+++ python app.py
@@ -0,0 +1,6 @@
+If I download python from the Python website and try to install it with the installer, it installs, but I need to specify that I want the new version of Python when I run my programs from terminal. If I just type:
+
+python app.py
+It runs with the old version.
+
+How do can I set the new version as default?
